25c1f5aae8ce4f275ca02451c3c687bde4cd7986
Joe Houghton <joe_houghton@live.co.uk>
2018-06-24 11:45:25 +0100

    Registration now Includes Domoticz config

--- PushoverRegistration.py
+++ PushoverRegistration.py
@@ -1,5 +1,17 @@
 from pushover_open_client import Client
 
+inputConfigFile = "base.cfg"
+outputConfigFile = "device.cfg"
+
+
+with open(inputConfigFile, 'r') as inputConfig:
+    jsonConfig = json.load(inputConfig)
+
+self.domoticzAddress = jsonConfig["domoticzAddress"]
+self.domoticzUser = jsonConfig["domoticzUser"]
+self.domoticzPassword = jsonConfig["domoticzPassword"]
+
+
 #Setup with a base config containing email and password
 client = Client("base.cfg")
 
@@ -7,8 +19,15 @@ client = Client("base.cfg")
 client.login()
 
 #Registers a new device using the supplied device name
-client.registerDevice("PushBulletDomoticzIFTTT")
+client.registerDevice("IFTTTDomoticz")
 
 #Save the new device to a new config so registration
 #can be bypassed in the future
-client.writeConfig("device.cfg")
+client.writeConfig(outputConfigFile)
+
+with open(outputConfigFile, 'r') as outputConfig:
+    jsonConfig = json.load(outputConfig)
+
+jsonConfig["domoticzAddress"] = domoticzAddress
+jsonConfig["domoticzUser"] = domoticzUser
+jsonConfig["domoticzPassword"] = domoticzPassword
